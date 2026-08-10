48b4bc76e5c30a49431fef490c290b2582739f30
YeThu531 <yethu20052@gmail.com>
2026-08-10 20:11:35 +0700

    Change API_KEY to a new value
    Updated the API key for Toncenter.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
    CONFIG
    ========================================================= */
 
-const API_KEY = "YOUR_TONCENTER_API_KEY";
+const API_KEY = "c09170dd62724a03f3803b0f1023219c672c0fcc02a2deed31bd75faea36e9e1";
 
 let tonweb = null;
 let walletData = JSON.parse(
